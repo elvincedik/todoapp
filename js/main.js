d799d300ffbf6fb0097061c55b8e03172af2e67f
evansgit config --global user.email you@example.com <you@example.com>
2022-11-06 16:01:42 +0100

    todo2

--- js/main.js
+++ js/main.js
@@ -1,51 +1,32 @@
-// var todoText = document.getElementById('todoText');
-// var addToDoButton = document.getElementById('addToDoButton');
-// var todoContent = document.getElementById('todoContent');
-
-// var todos = [];
-
-// addToDoButton.onclick = () => {
-//     todos.push({
-//         id: todos.length + 1,
-//         userId: 2,
-//         title: todoText.value,
-//         completed: false
-//     })
-//     displayTodos();
-// }
-
-// const addTodo = (text) => {
-    
-// }
-
-// const displayTodos = () => {
-//     let htmlStr = '<ul class="list-group">';
-
-//     for (let task of todos){
-//         htmlStr += `<li class="list-group-item">`
-//         htmlStr += `<input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">`
-//         htmlStr += `<label class="form-check-label" for="firstCheckbox">${task.title}</label>`
-//         htmlStr += `</li>`
-//     }
-//     htmlStr += '</ul>';
-//     todoText.value = '';
-//     todoContent.innerHTML = htmlStr;
-// }
-
-
 var todoText = document.getElementById('todoText');
 var addToDoButton = document.getElementById('addToDoButton');
 var todoContent = document.getElementById('todoContent');
 
-var todos = [];
+var todos = sessionStorage.getItem('todos') || [];
 
 addToDoButton.onclick = () => {
 let text = todoText.value;
 if(text != ''){
     addTodo(text)
 }
-
+attachEventListernersToTodoItem();
 }
+
+
+const attachEventListernersToTodoItem = () => {
+
+    let todocheckedboxes = document.querySelectorAll('input[type=checkbox]')
+
+    todocheckedboxes.forEach((todoItem, index) =>{
+
+        todoItem.onchange = () => {
+            todos[index].completed = !todos[index].completed
+            sessionStorage.setItem('todos', JSON.stringify(todos))
+        }
+
+    })
+}
+
 
 const addTodo = (text) => {
     todos.push({
@@ -54,6 +35,7 @@ const addTodo = (text) => {
         title: todoText.value,
         completed: false
     })
+    sessionStorage.setItem('todos', JSON.stringify(todos))
     displayTodos(); 
 }
 
@@ -62,12 +44,11 @@ const displayTodos = () => {
 
     for (let task of todos){
         htmlStr += `<li class="list-group-item">`
-        htmlStr += `<input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">`
-        htmlStr += `<label class="form-check-label" for="firstCheckbox">${task.title}</label>`
+        htmlStr += `<input class="form-check-input me-1" type="checkbox" ${task.completed ? 'checked' : ''} >`
+        htmlStr += `<label class="form-check-label" >${task.title}</label>`
         htmlStr += `</li>`
     }
     htmlStr += '</ul>';
     todoText.value = '';
     todoContent.innerHTML = htmlStr;
 }
-
